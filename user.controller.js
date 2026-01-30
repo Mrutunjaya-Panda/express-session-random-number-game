7e3f8a5feee61775386b141db469859619447aa3
Mrutunjaya Panda <mrutunjayapanda28@gmail.com>
2026-01-30 11:20:59 +0530

    added cookies attemptsLeft and randomNumber & hence created a very interesting gaming logic to guess the random input generated by the server

---
+++ user.controller.js
@@ -0,0 +1,89 @@
+import { handleLoginModel, handleSingUpModel } from "./user.model.js";
+
+export const handleGame = (req, res) => {
+  res.render("index", {
+    success: false,
+    message: null,
+    attemptsLeft: 5,
+    show: false,
+  });
+};
+
+export const handlePost = (req, res) => {
+  let { input } = req.body;
+  let { attemptsLeft } = req.cookies;
+  let { randomNumber } = req.cookies;
+
+  input = Number(input);
+  attemptsLeft = Number(attemptsLeft);
+  randomNumber = Number(randomNumber);
+  /* 🛑 STOP GAME IF ATTEMPTS ARE OVER */
+  if (attemptsLeft <= 0) {
+    return res.render("index", {
+      success: false,
+      show: true,
+      attemptsLeft: 0,
+      message: "Game over! Please reset the game.",
+      input,
+      randomNumber,
+    });
+  }
+
+  /* ⬇️ Decrement only if game is active */
+  attemptsLeft = attemptsLeft - 1;
+
+  res.cookie("attemptsLeft", attemptsLeft, {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+
+  if (randomNumber === input) {
+    res.render("index", {
+      success: true,
+      show: false,
+      attemptsLeft,
+      randomNumber,
+      input,
+    });
+  }
+
+  if (input < randomNumber) {
+    res.render("index", {
+      show: true,
+      success: false,
+      attemptsLeft,
+      message: "think of a larger number",
+      input,
+      randomNumber,
+    });
+  }
+
+  if (input > randomNumber) {
+    res.render("index", {
+      success: false,
+      show: true,
+      attemptsLeft,
+      message: "think of a smaller number",
+      input,
+      randomNumber,
+    });
+  }
+};
+
+export const renderLogin = (req, res) => {
+  res.render("login");
+};
+export const renderSignUp = (req, res) => {
+  res.render("signup");
+};
+export const handleSignUp = (req, res) => {
+  handleSingUpModel(req.body);
+  res.render("login");
+};
+export const handleLogin = (req, res) => {
+  const status = handleLoginModel(req.body);
+  if (status) {
+    //if true create a sessionID for that user
+    req.session.userEmail = req.body.email;
+    res.redirect("/");
+  } else res.json({ success: false, message: "Invalid Credentials!!" });
+};
